08eedfd2289e150a3607a03b58fac9c7c0aa88f8
Trần Thanh Tùng <tranthanhtung247@gmail.com>
2024-02-27 15:38:52 +0700

    Add files via upload

---
+++ labguide2/labguide2/Controllers/account.cs
@@ -0,0 +1,24 @@
+﻿using labguide2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace labguide2.Controllers
+{
+    public class account : Controller
+    {
+        public IActionResult Index()
+        {
+            List<account> acc = new List<account>();
+            account account = new account() { 
+            ID = 1, name = "tung", Phone = "0976727372", Gender = 1, Adress = "39 nguyen dinh chieu", Bio = "hpg", Birthday = "30/4/1975"
+
+                };
+
+            ViewBag.Account = account;
+
+        
+        
+                  return View();
+        }
+    }
+}
---
+++ labguide2/labguide2/Models/account.cs
@@ -0,0 +1,15 @@
+﻿namespace labguide2.Models
+{
+    public class account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Avatar { get; set; }
+        public string Adress { get; set; }
+        public string Bio { get; set; }
+        public string Gender { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
