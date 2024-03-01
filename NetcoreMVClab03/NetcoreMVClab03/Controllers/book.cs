a8184d5abc56fa1248ae1cc9ee4a1b6d406d25f7
Trần Thanh Tùng <tranthanhtung247@gmail.com>
2024-03-01 14:31:23 +0700

    Add files via upload

---
+++ NetcoreMVClab03/NetcoreMVClab03/Controllers/book.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetcoreMVClab03.Controllers
+{
+    public class book : Controller
+    { 
+        protected book book = new book();
+    {
+        public IActionResult Index()
+    {
+            //danh sach cac selectitem  chuyen qua combobox:
+            ViewBag.athors = book.authors;
+            ViewBag.generes = book.generes;
+            var books = book.getbooklist();
+    }
+        {
+           
+            return View(book);
+        }
+    }
+}
