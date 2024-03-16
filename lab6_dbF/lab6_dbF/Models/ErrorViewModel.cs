38b351b2dfe9c9b3d9b5646142e50bfe75a1d639
Trần Thanh Tùng <tranthanhtung247@gmail.com>
2024-03-16 09:28:47 +0700

    Add files via upload

---
+++ lab6_dbF/lab6_dbF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab6_dbF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ lab6_dbF/lab6_dbF/Program.cs
@@ -0,0 +1,35 @@
+namespace lab6_dbF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            var conectionstring = builder.Configuration.GetConnectionString("bookstoreconnectionstring");
+            
+.
+            var app = builder.Build();
+             
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
